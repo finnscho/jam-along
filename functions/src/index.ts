fbb3a4a2efaf4a17d517bda88d00e95f37de2407
Finn-Rasmus Knittermeier <finn-rasmus.knittermeier@bucher-suter.com>
2020-05-23 17:31:24 +0200

    pdate cordova

---
+++ functions/src/index.ts
@@ -0,0 +1,10 @@
+import * as functions from 'firebase-functions';
+// The Firebase Admin SDK to access Cloud Firestore.
+const admin = require('firebase-admin');
+admin.initializeApp();
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+export const helloWorld = functions.https.onRequest((request, response) => {
+ response.send("Hello from Firebase!");
+});
